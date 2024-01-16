b3b0f0c56e84e917261414cca1302517100f8b83
Lendysx16 <sarovgleb@gmail.com>
2024-01-16 08:45:00 +0300

    metadata without head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 //metadata
 import type { Metadata } from "next";
-import Head from "next/head";
 
 // styles, fonts
 import { Fira_Mono } from "next/font/google";
@@ -14,11 +13,24 @@ const mono = Fira_Mono({
     subsets: ["latin"],
     display: "swap",
 });
-// export const metadata: Metadata = {
-//     title: "Lendy | Portfolio",
-//     description:
-//         "Portfolio site for Lendy, a software engineer and designer-photographer.",
-// };
+export const metadata: Metadata = {
+    metadataBase: new URL("https://lendysx16.vercel.app"),
+    title: "Lendy | Portfolio",
+    description:
+        "Portfolio site for Lendy, a software engineer and designer-photographer.",
+    openGraph: {
+        title: "Lendy | Portfolio",
+        description:
+            "Portfolio site for Lendy, a software engineer and designer-photographer.",
+        images: [
+            {
+                url: "https://images.unsplash.com/photo-1705382575625-b4527151f50e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                width: 1200,
+                height: 630,
+            },
+        ],
+    },
+};
 
 export default function RootLayout({
     children,
@@ -27,16 +39,6 @@ export default function RootLayout({
 }) {
     return (
         <>
-            <Head>
-                <title>Lendysx16 | Portfolio </title>
-                <meta name="description" content="Portfolio site" />
-                <meta name="og:title" content="Lendysx16 | Portfolio" />
-                <meta name="og:description" content="Lendys site" />
-                <meta
-                    property="og:image"
-                    content="https://images.unsplash.com/photo-1705382575625-b4527151f50e?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                />
-            </Head>
             <html lang="en">
                 <body
                     className={
