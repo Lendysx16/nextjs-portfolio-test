ebbeae8c90a94569d09e3cea129614ccd7b7d741
Lendysx16 <sarovgleb@gmail.com>
2024-01-29 13:29:57 +0300

    projects page

--- next.config.js
+++ next.config.js
@@ -11,6 +11,17 @@ const nextConfig = {
             },
         ],
     },
+    redirects: async () => [
+        {
+            source: "/:path*",
+            has: [
+                { type: "host", value: "www.lendysx16.ru" },
+                { type: "host", value: "www.lendysx16.vercel.app" },
+            ],
+            destination: "https://lendysx16.ru/:path*",
+            permanent: true,
+        },
+    ],
 };
 
 module.exports = nextConfig;
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+import Wrapper from "@/components/Wrapper";
+import ProjectSection from "@/components/ProjectSectionElements/ProjectSection";
+const ProjectsPage = (): ReactNode => {
+    return (
+        <Wrapper>
+            <ProjectSection />
+        </Wrapper>
+    );
+};
+
+export default ProjectsPage;
