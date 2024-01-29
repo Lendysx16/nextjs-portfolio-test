8507984a0512670c7d3a02854dbf971e46ba7fce
Lendysx16 <sarovgleb@gmail.com>
2024-01-29 17:31:25 +0300

    use server-only function to fetch notion data

--- src/components/ProjectSectionElements/ProjectSection.tsx
+++ src/components/ProjectSectionElements/ProjectSection.tsx
@@ -1,37 +1,9 @@
+import { getProjectsData } from "@/utils/getProjectsData";
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import { ProjectCardData, NotionResponceType } from "@/types/types";
-import { Client } from "@notionhq/client";
-
-const getPojectsData = async (): Promise<ProjectCardData[]> => {
-  "server only";
-  const NOTION_KEY = process.env.NOTION_TOKEN;
-  const notion = new Client({ auth: NOTION_KEY });
-  const myProjects = await notion.databases.query({
-    database_id: process.env.NOTION_DATABASE_ID as string,
-  });
-  // @ts-ignore
-  const ArrWithProjects: ProjectCardData[] = [];
-  const results = myProjects.results;
-  // @ts-ignore
-  for (const { properties } of results) {
-    const data = properties as NotionResponceType;
-    ArrWithProjects.push({
-      title: data.Title.title[0].plain_text,
-      imgUrl: data.imgUrl.url,
-      gitUrl: data.gitUrl.url,
-      id: data.id.unique_id.prefix + data.id.unique_id.number.toString(),
-      tags: data.tags.multi_select.map((x) => x.name),
-      liveUrl: data.liveUrl.url,
-      description: data.Description.rich_text[0].plain_text,
-    });
-  }
-
-  return ArrWithProjects;
-};
 
 const ProjectSection = async () => {
-  const ProjectsData = await getPojectsData();
+  const ProjectsData = await getProjectsData();
   return (
     <>
       <h2 className=" mb-8 mt-4 text-center text-4xl font-bold text-white">
---
+++ src/utils/getProjectsData.ts
@@ -0,0 +1,32 @@
+import "server-only";
+
+import { ProjectCardData, NotionResponceType } from "@/types/types";
+import { Client } from "@notionhq/client";
+
+export const getProjectsData = async (): Promise<ProjectCardData[]> => {
+  const NOTION_KEY = process.env.NOTION_TOKEN;
+  const notion = new Client({ auth: NOTION_KEY });
+  const myProjects = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID as string,
+  });
+
+  // @ts-ignore
+  const ArrWithProjects: ProjectCardData[] = [];
+  const results = myProjects.results;
+
+  // @ts-ignore
+  for (const { properties } of results) {
+    const data = properties as NotionResponceType;
+    ArrWithProjects.push({
+      title: data.Title.title[0].plain_text,
+      imgUrl: data.imgUrl.url,
+      gitUrl: data.gitUrl.url,
+      id: data.id.unique_id.prefix + data.id.unique_id.number.toString(),
+      tags: data.tags.multi_select.map((x) => x.name),
+      liveUrl: data.liveUrl.url,
+      description: data.Description.rich_text[0].plain_text,
+    });
+  }
+
+  return ArrWithProjects;
+};
